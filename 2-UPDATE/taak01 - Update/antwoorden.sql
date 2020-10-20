df1c7b3bf96c8fb5e72788a8edad36e4b6ecd5ef
DaanDeJager <daandejager2002@gmail.com>
2020-10-20 19:59:50 +0200

    done

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,19 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET woonplaats = 'Amstelveen';
 -- Opdracht 2 
-
+UPDATE studenten2
+SET adres = 'Ebro', postcode = '1234VE', klas = '9C'
+WHERE adres = '';
 -- Opdracht 3
-
+UPDATE studenten2
+SET geboortedatum = '2000-12-12'
+WHERE voornaam = 'Marloes';
 -- Opdracht 4
-
+UPDATE studenten2
+SET klas = '9C'
+WHERE voornaam = 'Mohammed';
 -- Opdracht 5
+UPDATE studenten2 
+SET voornaam = 'Johannah' 
+WHERE voornaam = 'Johanna'
